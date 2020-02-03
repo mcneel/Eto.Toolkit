afbad5fdc4f0b8e937d24e02e416304b42882c9d
Alain <alain@mcneel.com>
2020-02-03 15:32:35 +0100

    stash

--- src/Eto.CodeEditor.XamMac/CodeEditorHandler.cs
+++ src/Eto.CodeEditor.XamMac/CodeEditorHandler.cs
@@ -17,11 +17,6 @@ namespace Eto.CodeEditor
 {
     public partial class CodeEditorHandler : Eto.Mac.Forms.MacView<Scintilla.ScintillaControl, CodeEditor, CodeEditor.ICallback>, CodeEditor.IHandler
     {
-        private const int BREAKPOINT_MARKER = 3; // arbitrary number
-        private const int BREAK_MARKER = 4; // arbitrary number
-
-        private const int BREAKPOINTS_MARGIN = 1;
-        private const int LINENUMBERS_MARGIN = 2;
         static CodeEditorHandler()
         {
             var path = Path.Combine(NSBundle.MainBundle.PrivateFrameworksPath, "Scintilla.framework", "Scintilla");
@@ -47,29 +42,29 @@ namespace Eto.CodeEditor
             ShowIndentationGuides();
             Control.Message(NativeMethods.SCI_AUTOCSETMAXHEIGHT, new IntPtr(10), IntPtr.Zero);
 
-            // breakpoints margin
-            Control.SetGeneralProperty(NativeMethods.SCI_SETMARGINSENSITIVEN, BREAKPOINTS_MARGIN, 1);
-            Control.SetGeneralProperty(NativeMethods.SCI_SETMARGINTYPEN, BREAKPOINTS_MARGIN, NativeMethods.SC_MARGIN_SYMBOL);
-            Control.SetGeneralProperty(NativeMethods.SCI_SETMARGINMASKN, BREAKPOINTS_MARGIN, (nint)uint.MaxValue); // ScintillaNet -> public const uint MaskAll = unchecked((uint)-1);
-            Control.SetGeneralProperty(NativeMethods.SCI_MARKERDEFINE, BREAKPOINTS_MARGIN, NativeMethods.SC_MARK_FULLRECT);
-            IsBreakpointsMarginVisible = false;
+            //// breakpoints margin
+            //Control.SetGeneralProperty(NativeMethods.SCI_SETMARGINSENSITIVEN, BREAKPOINTS_MARGIN, 1);
+            //Control.SetGeneralProperty(NativeMethods.SCI_SETMARGINTYPEN, BREAKPOINTS_MARGIN, NativeMethods.SC_MARGIN_SYMBOL);
+            //Control.SetGeneralProperty(NativeMethods.SCI_SETMARGINMASKN, BREAKPOINTS_MARGIN, (nint)uint.MaxValue); // ScintillaNet -> public const uint MaskAll = unchecked((uint)-1);
+            //Control.SetGeneralProperty(NativeMethods.SCI_MARKERDEFINE, BREAKPOINTS_MARGIN, NativeMethods.SC_MARK_FULLRECT);
+            //IsBreakpointsMarginVisible = false;
 
-            // line numbers margin
-            Control.SetGeneralProperty(NativeMethods.SCI_SETMARGINSENSITIVEN, LINENUMBERS_MARGIN, 0);
-            Control.SetGeneralProperty(NativeMethods.SCI_SETMARGINTYPEN, LINENUMBERS_MARGIN, NativeMethods.SC_MARGIN_NUMBER);
-            //Control.SetGeneralProperty(NativeMethods.SCI_SETMARGINMASKN, BREAKPOINTS_MARGIN, (nint)uint.MaxValue);
+            //// line numbers margin
+            //Control.SetGeneralProperty(NativeMethods.SCI_SETMARGINSENSITIVEN, LINENUMBERS_MARGIN, 0);
+            //Control.SetGeneralProperty(NativeMethods.SCI_SETMARGINTYPEN, LINENUMBERS_MARGIN, NativeMethods.SC_MARGIN_NUMBER);
+            ////Control.SetGeneralProperty(NativeMethods.SCI_SETMARGINMASKN, BREAKPOINTS_MARGIN, (nint)uint.MaxValue);
 
-            // breakpoint marker
-            Control.SetGeneralProperty(NativeMethods.SCI_MARKERDEFINE, BREAKPOINT_MARKER, NativeMethods.SC_MARK_CIRCLE); // default
-            var red = 255; // 0xFF0000; // */ 16711680;
-            Control.SetGeneralProperty(NativeMethods.SCI_MARKERSETFORE, BREAKPOINT_MARKER, red);
-            Control.SetGeneralProperty(NativeMethods.SCI_MARKERSETBACK, BREAKPOINT_MARKER, red);
+            //// breakpoint marker
+            //Control.SetGeneralProperty(NativeMethods.SCI_MARKERDEFINE, BREAKPOINT_MARKER, NativeMethods.SC_MARK_CIRCLE); // default
+            //var red = 255; // 0xFF0000; // */ 16711680;
+            //Control.SetGeneralProperty(NativeMethods.SCI_MARKERSETFORE, BREAKPOINT_MARKER, red);
+            //Control.SetGeneralProperty(NativeMethods.SCI_MARKERSETBACK, BREAKPOINT_MARKER, red);
 
-            // break marker
-            Control.SetGeneralProperty(NativeMethods.SCI_MARKERDEFINE, BREAK_MARKER, NativeMethods.SC_MARK_ARROW);
-            var yellow = 0x00FFFF; // */ 16776960;
-            Control.SetGeneralProperty(NativeMethods.SCI_MARKERSETFORE, BREAK_MARKER, 0xFFFFFF); //black
-            Control.SetGeneralProperty(NativeMethods.SCI_MARKERSETBACK, BREAK_MARKER, yellow);
+            //// break marker
+            //Control.SetGeneralProperty(NativeMethods.SCI_MARKERDEFINE, BREAK_MARKER, NativeMethods.SC_MARK_ARROW);
+            //var yellow = 0x00FFFF; // */ 16776960;
+            //Control.SetGeneralProperty(NativeMethods.SCI_MARKERSETFORE, BREAK_MARKER, 0xFFFFFF); //black
+            //Control.SetGeneralProperty(NativeMethods.SCI_MARKERSETBACK, BREAK_MARKER, yellow);
         }
 
         //public string Text
@@ -513,34 +508,7 @@ namespace Eto.CodeEditor
         unsafe void NotificationProtocol_Notify(object sender, SCNotifyEventArgs e)
         {
             var n = e.Notification;
-            switch (n.nmhdr.code)
-            {
-
-                case NativeMethods.SCN_CHARADDED:
-                    CharAdded?.Invoke(this, new CharAddedEventArgs((char)n.ch));
-                    break;
-                case NativeMethods.SCN_MODIFIED:
-                    if ((n.modificationType & NativeMethods.SC_MOD_INSERTCHECK) > 0)
-                    {
-                        var text = Helpers.GetString(n.text, (int)n.length, Encoding);
-                        InsertCheck?.Invoke(this, new InsertCheckEventArgs(text));
-                    }
-                    TextChanged?.Invoke(this, EventArgs.Empty);
-                    break;
-                case NativeMethods.SCN_MARGINCLICK:
-                    const uint bmmask = (1 << BREAKPOINT_MARKER);
-                    var lineNumber = Control.GetGeneralProperty(NativeMethods.SCI_LINEFROMPOSITION, (nint)n.position);
-                    var mask = (int)(Control.GetGeneralProperty(NativeMethods.SCI_MARKERGET, lineNumber));
-                    var uimask = unchecked((uint)mask);
-                    var addOrRemove = ((uimask & bmmask) > 0) ? BreakpointChangeType.Remove : BreakpointChangeType.Add;
-                    if (addOrRemove == BreakpointChangeType.Add && string.IsNullOrWhiteSpace(GetLineText((int)lineNumber)))
-                        return;
-                    Control.SetGeneralProperty(addOrRemove == BreakpointChangeType.Add ? NativeMethods.SCI_MARKERADD : NativeMethods.SCI_MARKERDELETE, lineNumber, BREAKPOINT_MARKER);
-                    BreakpointsChanged?.Invoke(this, new BreakpointsChangedEventArgs(addOrRemove, (int)lineNumber));
-                    break;
-                default:
-                    break;
-            }
+            Control.HandleScintillaMessage((int)n.nmhdr.code, (char)n.ch, (int)n.position);
         }
 
         //public void ClearBreakpoints()
@@ -557,17 +525,12 @@ namespace Eto.CodeEditor
 
         //public void ClearBreak() => Control.SetGeneralProperty(NativeMethods.SCI_MARKERDELETEALL, BREAK_MARKER);
 
-        public event EventHandler<CharAddedEventArgs> CharAdded;
-        public event EventHandler<EventArgs> TextChanged;
-        public event EventHandler<BreakpointsChangedEventArgs> BreakpointsChanged;
-        public event EventHandler<InsertCheckEventArgs> InsertCheck;
-
-        public unsafe void ChangeInsertion(string text)
-        {
-            var bytes = Helpers.GetBytes(text ?? string.Empty, Encoding, zeroTerminated: false);
-            fixed (byte* bp = bytes)
-            Control.Message(NativeMethods.SCI_CHANGEINSERTION, new IntPtr(bytes.Length), new IntPtr(bp));
-        }
+        //public unsafe void ChangeInsertion(string text)
+        //{
+        //    var bytes = Helpers.GetBytes(text ?? string.Empty, Encoding, zeroTerminated: false);
+        //    fixed (byte* bp = bytes)
+        //    Control.Message(NativeMethods.SCI_CHANGEINSERTION, new IntPtr(bytes.Length), new IntPtr(bp));
+        //}
 
         //private int MarkerNext(int lineNumber) => (int)Control.GetGeneralProperty(NativeMethods.SCI_MARKERNEXT, lineNumber, 1 << BREAKPOINT_MARKER); // NativeMethods.SC_MARK_CIRCLE);
         //public IEnumerable<int> Breakpoints
@@ -596,62 +559,62 @@ namespace Eto.CodeEditor
         }
 
 
-        static int[] CommentStyleIds(ProgrammingLanguage language)
-        {
-            if (language == ProgrammingLanguage.Python)
-                return new int[] { NativeMethods.SCE_P_COMMENTBLOCK, NativeMethods.SCE_P_COMMENTLINE };
+        //static int[] CommentStyleIds(ProgrammingLanguage language)
+        //{
+        //    if (language == ProgrammingLanguage.Python)
+        //        return new int[] { NativeMethods.SCE_P_COMMENTBLOCK, NativeMethods.SCE_P_COMMENTLINE };
 
-            if (language == ProgrammingLanguage.VB)
-                return new int[] { NativeMethods.SCE_B_COMMENT, NativeMethods.SCE_B_COMMENTBLOCK,
-                    NativeMethods.SCE_B_DOCBLOCK};
+        //    if (language == ProgrammingLanguage.VB)
+        //        return new int[] { NativeMethods.SCE_B_COMMENT, NativeMethods.SCE_B_COMMENTBLOCK,
+        //            NativeMethods.SCE_B_DOCBLOCK};
 
-            return new int[] { NativeMethods.SCE_C_COMMENT, NativeMethods.SCE_C_COMMENTLINE,
-                NativeMethods.SCE_C_COMMENTDOC, NativeMethods.SCE_C_COMMENTLINEDOC };
-        }
+        //    return new int[] { NativeMethods.SCE_C_COMMENT, NativeMethods.SCE_C_COMMENTLINE,
+        //        NativeMethods.SCE_C_COMMENTDOC, NativeMethods.SCE_C_COMMENTLINEDOC };
+        //}
 
-        static int[] StringStyleIds(ProgrammingLanguage language)
-        {
-            if (language == ProgrammingLanguage.Python)
-                return new int[] { NativeMethods.SCE_P_CHARACTER, NativeMethods.SCE_P_STRING,
-                    NativeMethods.SCE_P_TRIPLE, NativeMethods.SCE_P_TRIPLEDOUBLE };
+        //static int[] StringStyleIds(ProgrammingLanguage language)
+        //{
+        //    if (language == ProgrammingLanguage.Python)
+        //        return new int[] { NativeMethods.SCE_P_CHARACTER, NativeMethods.SCE_P_STRING,
+        //            NativeMethods.SCE_P_TRIPLE, NativeMethods.SCE_P_TRIPLEDOUBLE };
 
-            if (language == ProgrammingLanguage.VB)
-                return new int[] { NativeMethods.SCE_B_STRING };
-            return new int[] { NativeMethods.SCE_C_STRING, NativeMethods.SCE_C_CHARACTER };
-        }
+        //    if (language == ProgrammingLanguage.VB)
+        //        return new int[] { NativeMethods.SCE_B_STRING };
+        //    return new int[] { NativeMethods.SCE_C_STRING, NativeMethods.SCE_C_CHARACTER };
+        //}
 
-        static int[] Keyword1Ids(ProgrammingLanguage language)
-        {
-            if (language == ProgrammingLanguage.Python)
-                return new int[] { NativeMethods.SCE_P_WORD };
+        //static int[] Keyword1Ids(ProgrammingLanguage language)
+        //{
+        //    if (language == ProgrammingLanguage.Python)
+        //        return new int[] { NativeMethods.SCE_P_WORD };
 
-            if (language == ProgrammingLanguage.VB)
-                return new int[] { NativeMethods.SCE_B_KEYWORD };
+        //    if (language == ProgrammingLanguage.VB)
+        //        return new int[] { NativeMethods.SCE_B_KEYWORD };
 
-            return new int[] { NativeMethods.SCE_C_WORD };
-        }
+        //    return new int[] { NativeMethods.SCE_C_WORD };
+        //}
 
-        static int[] Keyword2Ids(ProgrammingLanguage language)
-        {
-            if (language == ProgrammingLanguage.Python)
-                return new int[] { NativeMethods.SCE_P_WORD2 };
+        //static int[] Keyword2Ids(ProgrammingLanguage language)
+        //{
+        //    if (language == ProgrammingLanguage.Python)
+        //        return new int[] { NativeMethods.SCE_P_WORD2 };
 
-            if (language == ProgrammingLanguage.VB)
-                return new int[] { NativeMethods.SCE_B_KEYWORD2, NativeMethods.SCE_B_KEYWORD3, NativeMethods.SCE_B_KEYWORD4 };
+        //    if (language == ProgrammingLanguage.VB)
+        //        return new int[] { NativeMethods.SCE_B_KEYWORD2, NativeMethods.SCE_B_KEYWORD3, NativeMethods.SCE_B_KEYWORD4 };
 
-            return new int[] { NativeMethods.SCE_C_WORD2 };
-        }
+        //    return new int[] { NativeMethods.SCE_C_WORD2 };
+        //}
 
-        static int[] PreprocessorIds(ProgrammingLanguage language)
-        {
-            if (language == ProgrammingLanguage.Python)
-                return new int[] { };
+        //static int[] PreprocessorIds(ProgrammingLanguage language)
+        //{
+        //    if (language == ProgrammingLanguage.Python)
+        //        return new int[] { };
 
-            if (language == ProgrammingLanguage.VB)
-                return new int[] { NativeMethods.SCE_B_PREPROCESSOR };
+        //    if (language == ProgrammingLanguage.VB)
+        //        return new int[] { NativeMethods.SCE_B_PREPROCESSOR };
 
-            return new int[] { NativeMethods.SCE_C_PREPROCESSOR };
-        }
+        //    return new int[] { NativeMethods.SCE_C_PREPROCESSOR };
+        //}
 
     }
 }
